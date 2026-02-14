c175ace9063ca48b9fc097741fc320747c6fda72
Kyle <dellocky1@gmail.com>
2026-02-14 18:39:07 -0500

    Inital Directory Structure

--- Main.java
+++ Main.java
@@ -1,11 +1,34 @@
+import java.io.File;                  
+import java.io.FileNotFoundException; 
+import java.util.Scanner;      
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Main {
-public static void main(String[] args) {
-	InputFile configFile = new InputFile("input.txt");
-	configFile.process();
+	public static void main(String[] args) {
+
+		//TODO connect Map to factoires
+		//Map<String, factory> dict = new HashMap<>();
+
+		String path = "input.txt";
+		File configFile = new File(path);
+		try (Scanner myReader = new Scanner(configFile)) {
+			while (myReader.hasNextLine()) {
+				String line = myReader.nextLine();
+				line = line.strip();
+				//TODO connect methods from abstract factory to tokens
+				System.out.println(line);
+				}
+			}
+			catch (FileNotFoundException e) {
+				System.out.println("An error occurred, please ensure correct input file path");
+				//e.printStackTrace();
+			
+		}
+	}
 }
-}
+
 
 
 
